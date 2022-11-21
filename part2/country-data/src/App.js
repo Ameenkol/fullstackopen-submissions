11d260c203c41a2538bfd6152fdf8953580554d9
Ameenkol <garbakolameen@gmail.com>
2022-11-21 08:54:01 +0100

    Part2- Exercise 2.13

--- part2/country-data/src/App.js
+++ part2/country-data/src/App.js
@@ -7,15 +7,17 @@ import List from './List';
 function App() {
   const [list, setList] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+  const [show, setShow] = useState(false)
+  const [filterResult, setFilterResult] = useState([])
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
     .then(res => {
-        setList(res.data)
-      })
-  
+      setList(res.data)
+    })
+    
   }, [])
-  
+
   const handleChange = (e) => {
     let lowerCase = e.target.value.toLowerCase()
     setSearchTerm(lowerCase)
@@ -29,8 +31,16 @@ function App() {
     return country.name.common.toLowerCase().includes(searchTerm)
   })
 
+
+  const showDetails = (i) => {
+    setShow(true)
+    const countryToDisplay = countryToShow[i];
+    setFilterResult(countryToDisplay)
+  }
+
   return (
     <div className="App">
+
       <h2>Find Countries</h2>
       <div style={{maxHeight: 'fit-content', overflow:'auto'}}>
         <Input
@@ -41,7 +51,12 @@ function App() {
       </div>
 
       <h3>Country List</h3>
-      <List countryToShow={countryToShow} />
+      <List
+        countryToShow={countryToShow}
+        showDetails={showDetails}
+        show={show}
+        filterResult={filterResult}
+        />
     </div>
   );
 }
--- part2/country-data/src/List.js
+++ part2/country-data/src/List.js
@@ -1,20 +1,50 @@
-import React from 'react'
 
-const List = ({countryToShow}) => {
+const List = ({ countryToShow, showDetails, show, filterResult }) => {
+
   return (
      <div style={{maxHeight: 'fit-content', listStyle:'none'}}>
         {countryToShow.length > 10
-          ?
+        ?
+        
           <p>Too many matches, specify another filter</p>
-          :
+        :
+        
           (countryToShow.length > 1
+          ?
+          
+          (show === false
             ?
-            countryToShow.map((country) => (
-          <div key={country.capital + country.name.common}>
-              <h2>{country.name.common}</h2>
-            </div>
-          ))
+            
+            countryToShow.map((country, i) => (
+              <li
+                key={country.capital[0] + country.name.common + i}
+              >
+                  <p>{country.name.common}{' '}
+                    <button
+                      onClick={(e) => showDetails(i)}>show</button>
+                  </p>
+                </li>
+            ))
             :
+
+            <div>
+              <h2>{filterResult.name.common}</h2>
+              <p>{filterResult.capital}</p>
+              <p>{filterResult.area}</p>
+
+              <h4>Languages</h4>
+                <ul>
+                  <li>
+                    {Object.values(filterResult.languages)}             
+                  </li>
+                </ul>
+              <>
+              <img src={filterResult.flags.png} alt='flag'/>
+            </>
+            </div>
+          )
+          :
+          
           countryToShow.map((country) => {
           let keys = {}
 
